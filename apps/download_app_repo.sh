a8a8cc37fbb409ace74b6fba66ca0b810f0d3752
zhiyuli <zyli2004@gmail.com>
2017-01-30 11:39:04 -0700

    update repo_list

--- apps/download_app_repo.sh
+++ apps/download_app_repo.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-while read repo; do
-    git clone $repo
-done < repo_list.txt
+if [  $# -eq 0 ]
+then
+    echo "No repo list given, reading default list (repo_list.txt)..."
+    while read repo; do
+        git clone $repo
+    done < repo_list.txt
+else
+    while read repo; do
+        git clone $repo
+    done < $1
+fi
